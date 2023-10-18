52c620539ffd9fe9646d32d39937e12822e9ea38
Helloisa22 <helloisa.betriz44@gmail.com>
2023-10-18 20:34:40 -0300

    Aula JS 02 continuacao

--- JS/Aula02-Variaveis/main.js
+++ JS/Aula02-Variaveis/main.js
@@ -48,7 +48,57 @@ var osDeVerdadeAGenteSabeQuemE = false;
 
 console.clear()
 
-
-
-
 console.log("five" * 2)
+console.clear()
+
+// Incremento
+var incremento = 2;
+
+// incremento = incremento + 6
+incremento+=6
+
+console.log(incremento)
+
+console.clear()
+
+
+var imparPar = 3%2
+
+console.log("Se o resultado for 1 então o número é impar se o resultado for 0 então é par, esse é o resultado: " + imparPar)
+
+console.clear()
+
+
+
+var comparacao = 2 < 3
+
+console.log("2 é menor que 3? " + comparacao) // true  / false
+
+
+console.clear()
+
+
+var doisSinaisDeIgual = 3 == "três"
+
+console.log(doisSinaisDeIgual)
+
+// texto, numero
+// ===
+// 3 == 5 // true   ou
+// numerico = numerico // false
+
+console.clear()
+
+var addNumero = "2";
+
+//addNumero = addNumero + 4;
+// addNumero = 
+
+console.log(typeof addNumero)
+
+
+
+
+
+
+
