d84fdc3df936b0a203ed3cb9f76eaf69b30f2a67
Helloisa22 <helloisa.betriz44@gmail.com>
2023-10-17 21:57:46 -0300

    aula 02 JS

---
+++ JS/Aula02-Variaveis/main.js
@@ -0,0 +1,54 @@
+var nomeDoPrimeroAluno = "Felipe"
+
+console.log(nomeDoPrimeroAluno);
+
+let numeroDeAprovados = 5
+
+console.log(numeroDeAprovados)
+
+console.clear()
+
+const nomeSiteWeb = "facebook"
+
+const nomeSiteWeb2 = "G"
+
+console.log(nomeSiteWeb)
+console.log(nomeSiteWeb2)
+
+// Exibir a soma de dois números
+//console.log(0 / 0)
+
+/*var numero1 = 6; numerico
+
+var numero2 = 4; numerico
+
+var soma = numero1 + numero2;
+
+console.log(soma)*/
+
+
+
+// String
+var texto = 'Pensando mais a longo prazo, a constante divulgação das informações ainda não demonstrou convincentemente'
+
+console.log(texto)
+
+console.clear()
+
+var texto1 = "Pensando mais a longo prazo, a constante " // String
+
+var texto2 = "divulgação das informações ainda não demonstrou convincentemente \" Pensando mais a longo prazo, a constante" // String
+
+var concatenar = texto1 + texto2;
+
+console.log(concatenar)
+
+console.clear()
+var osDeVerdadeAGenteSabeQuemE = false;
+
+console.clear()
+
+
+
+
+console.log("five" * 2)
