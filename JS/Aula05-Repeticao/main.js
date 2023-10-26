d77cfaf8169996c61c01b81a90f30bd18c820812
Helloisa22 <helloisa.betriz44@gmail.com>
2023-10-25 21:36:08 -0300

    continuacao aula 05

---
+++ JS/Aula05-Repeticao/main.js
@@ -0,0 +1,23 @@
+// // Laço While
+// let contador = 1
+
+// while(contador <= 10){ // true
+//     console.log(`O número do contador é  
+//     ${contador}`)
+//     contador++;
+// }
+
+for(let contador = 1; contador <= 10; contador++){
+    console.log(`O número do contador é  
+    ${contador}`)
+}
+
+
+
+// do{
+//     debugger
+//     console.log(`O número do contador é  
+//     ${contador}`)
+//     contador++;
+// }while(contador <= 10)
+
