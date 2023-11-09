ca2389584910db9f4ad8898fdfd3719ce1efbae1
Helloisa22 <helloisa.betriz44@gmail.com>
2023-11-08 21:33:15 -0300

    Aula 10 DOM

---
+++ JS/Aula11-DOM-Parte1/main.js
@@ -0,0 +1,11 @@
+// criacao da variavel e criacao do botao
+let botao = document.createElement("BUTTON");
+let corpo = document.body;
+// adicionar o filho(botao) na body
+document.body.appendChild(botao)
+// Adiciona um texto dentro do botao
+botao.innerHTML = 'Acessa ai';
+// adicionei estilo ao botao
+corpo.style.backgroundColor = 'green'
+botao.style.border = 'solid 2px blue';
+
