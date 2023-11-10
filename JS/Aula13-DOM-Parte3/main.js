2c7c490cf2e9066ffe1ebe4177ab8f05461d8539
Helloisa22 <helloisa.betriz44@gmail.com>
2023-11-10 20:29:40 -0300

    Aula 13 DOM Parte 3

---
+++ JS/Aula13-DOM-Parte3/main.js
@@ -0,0 +1,50 @@
+// var pipoca = document.querySelectorAll('p');
+// console.log(pipoca)
+
+// // Acesse todos os paragrafos e mude a cor deles
+// let c = 0;
+// while(c < pipoca.length){
+//     pipoca[c].style.color = 'purple'
+//     c++
+// }
+
+
+// let j = document.querySelector('p.exemplo')
+// j.innerHTML = 'Foi alterado aqui o texto com as lagrimas dos alunos'
+// j.style.color = 'blue'
+
+
+// let y = document.querySelector('#exemplo2').style.color = 'yellow'
+
+
+
+
+// function Nome(){
+//     var entradaNome = prompt('Insira o seu nome')
+//     let mostraNome = document.querySelector('#mostraNome')
+//     console.log(entradaNome)
+
+//     mostraNome.innerHTML = `Olá ${entradaNome} Bem-vindo a nossa academia`
+// }
+
+// const Nome = () => {
+//     let recebeNome = document.querySelector('#recebeNome')
+//     let mostraNome = document.querySelector('#mostraNome')
+
+//     console.log(recebeNome)
+
+//     mostraNome.innerHTML = `Olá ${recebeNome.value} Bem-vindo a nossa academia`
+// }
+let evento = document.querySelector('#chamaFuncao')
+
+const Nome = () =>{
+    let recebeNome = document.querySelector('#recebeNome')
+    let mostraNome = document.querySelector('#mostraNome')
+
+    mostraNome.innerHTML = `Olá ${recebeNome.value} Bem-vindo a nossa academia`
+
+}
+
+evento.addEventListener('click', Nome)
+// O nome do evento, callback(funcao), opcional true/false
+
